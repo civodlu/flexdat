f6edfd385b1c32d0e67bbe0421e4368a06b48cfb
civodlu <civodlu@gmail.com>
2024-05-23 13:56:04 +0200

    - breaking: changed to non deterministic sampling - added support for multiple base datasets

--- src/flexdat/dataset_virtual_resize.py
+++ src/flexdat/dataset_virtual_resize.py
@@ -1,5 +1,7 @@
 import logging
-from typing import Dict, Optional
+from typing import Dict, Literal, Optional, Sequence, Union
+
+import numpy as np
 
 from .dataset import CoreDataset
 from .types import Batch
@@ -18,16 +20,37 @@ class DatasetResizeVirtual(CoreDataset):
     >>> dataset = DatasetPath(paths)
     >>> dataset = DatasetResizeVirtual(dataset, size=10)
     >>> assert dataset[3]['path'] == '/path/1'
+
+    Optionally, multiple datasets can be supported.
     """
 
-    def __init__(self, base_dataset: CoreDataset, size: int):
+    def __init__(
+        self,
+        base_dataset: Union[CoreDataset, Sequence[CoreDataset]],
+        size: int,
+        sampling_mode: Literal['sequential'] = 'sequential',
+    ):
+        """
+        Args:
+            sampling_mode: `sequential` will sample from each base_datasets sequentially
+        """
         super().__init__()
-        self.base_dataset = base_dataset
+
+        if not isinstance(base_dataset, Sequence):
+            base_dataset = [base_dataset]
+        self.base_datasets = base_dataset
         self.size = size
+        self.sampling_mode = sampling_mode
 
     def __len__(self) -> int:
         return self.size
 
     def __getitem__(self, index: int, context: Optional[Dict] = None) -> Optional[Batch]:
-        base_index = index % len(self.base_dataset)
-        return self.base_dataset.__getitem__(base_index, context)
+        assert index < self.size
+        base_dataset_index = index % len(self.base_datasets)
+        if self.sampling_mode == 'sequential':
+            base_dataset = self.base_datasets[base_dataset_index]
+            base_index = np.random.randint(0, len(base_dataset))
+            return base_dataset.__getitem__(base_index, context)
+
+        raise ValueError(f'unsupported sampling={self.sampling_mode}!')
--- tests/test_dataset_virtual_resize.py
+++ tests/test_dataset_virtual_resize.py
@@ -7,3 +7,26 @@ def test_virtual_resize():
     dataset = DatasetResizeVirtual(dataset, size=10)
     assert dataset[3]['path'] == '/path/1'
     assert len(dataset) == 10
+
+
+def test_virtual_resize_multiple_datasets_sequential_sampling():
+    paths_1 = ['/path/1', '/path/2', '/path/3']
+    dataset_1 = DatasetPath(paths_1)
+
+    paths_2 = ['/path/4', '/path/5']
+    dataset_2 = DatasetPath(paths_2)
+
+    dataset = DatasetResizeVirtual([dataset_1, dataset_2], size=50, sampling_mode='sequential')
+    paths = [paths_1, paths_2]
+    assert len(dataset) == 50
+
+    paths_results = [set(), set()]
+    for i in range(50):
+        b = dataset[i]
+        expected_dataset = i % 2
+        assert b['path'] in paths[expected_dataset]
+        paths_results[expected_dataset].add(b['path'])
+
+    # make sure we sampled EVERYTHING
+    assert paths_results[0] == set(paths[0])
+    assert paths_results[1] == set(paths[1])
